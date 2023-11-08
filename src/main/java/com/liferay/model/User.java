d640e98bc4795039452374bb7eb572c96a8037de
Yang Cao <yang.cao@liferay.com>
2023-11-09 05:57:04 +0800

    Add use model class

---
+++ src/main/java/com/liferay/model/User.java
@@ -0,0 +1,27 @@
+package com.liferay.model;
+
+public class User {
+    public User(String emailAddress, String password) {
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    private String emailAddress;
+    private String password;
+}
