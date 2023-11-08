8fcad675933daa03d2b01e2080f85288f1a212ef
Yang Cao <yang.cao@liferay.com>
2023-11-09 07:02:01 +0800

    Add test to add user with invalid password

---
+++ src/test/java/com/liferay/examples/ExceptionTest.java
@@ -0,0 +1,16 @@
+package com.liferay.examples;
+
+import com.liferay.model.User;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
+public class ExceptionTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddUserWithInvalidPassword() {
+        User user = new User("test@liferay.com", "a");
+
+        assertNull(user);
+    }
+}
