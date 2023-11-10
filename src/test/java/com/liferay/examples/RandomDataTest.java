fc17eda8a3dc0609cc2946ed2a8bd3c55d809e18
Yang Cao <yang.cao@liferay.com>
2023-11-10 18:35:17 +0800

    Add more tests

--- src/test/java/com/liferay/examples/RandomDataTest.java
+++ src/test/java/com/liferay/examples/RandomDataTest.java
@@ -1,4 +1,18 @@
 package com.liferay.examples;
 
+import org.junit.Test;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class RandomDataTest {
+
+    @Test
+    public void testGenerateRandomPassword() {
+        String password = RandomStringUtils.randomAlphanumeric(10);
+
+        assertEquals(10, password.length());
+        assertTrue(password.matches("^[A-Za-z0-9]+$"));
+    }
 }
--- src/test/java/com/liferay/examples/StringTest.java
+++ src/test/java/com/liferay/examples/StringTest.java
@@ -1,4 +1,21 @@
 package com.liferay.examples;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringTest {
+
+    @Test
+    public void testBuildString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("> ");
+        sb.append(1);
+        sb.append(" + ");
+        sb.append(2);
+        char[] ca = {' ', '=', ' ', '3'};
+        sb.append(ca);
+
+        assertEquals("> 1 + 2 = 3", sb.toString());
+    }
 }
