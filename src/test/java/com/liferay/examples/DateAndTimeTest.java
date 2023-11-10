a35900775f6c818964ef15f17e380cd6200eed40
Yang Cao <yang.cao@liferay.com>
2023-11-10 18:35:17 +0800

    Add more tests

--- src/test/java/com/liferay/examples/DateAndTimeTest.java
+++ src/test/java/com/liferay/examples/DateAndTimeTest.java
@@ -1,4 +1,22 @@
 package com.liferay.examples;
 
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertEquals;
+
 public class DateAndTimeTest {
+
+    @Test
+    public void testFormatDateAndTime() {
+        Date date = new Date(0L);
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+
+        simpleDateFormat.applyPattern("MMM/dd/yyyy HH:mm");
+
+        assertEquals("Jan/01/1970 08:00", simpleDateFormat.format(date));
+    }
 }
