7ce955bccefc0e20b40459ffee9994593f2a95d4
Yang Cao <yang.cao@liferay.com>
2023-11-10 16:04:50 +0800

    Add tests to AnnotationsTests

--- src/test/java/com/liferay/examples/AnnotationsTest.java
+++ src/test/java/com/liferay/examples/AnnotationsTest.java
@@ -1,4 +1,41 @@
 package com.liferay.examples;
 
+import org.junit.*;
+
 public class AnnotationsTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        System.out.println("Run once, before any @Test methods are run");
+    }
+    @Before
+    public void setUp() {
+        System.out.println(" Run before each @Test method");
+    }
+
+    @Test
+    public void testHelloWorld() {
+        System.out.println("Hello World");
+    }
+    @Test
+    public void testWelcomeToLiferay() {
+        System.out.println("Welcome to Liferay");
+    }
+
+    @Ignore("Prevent a @Test method from running")
+    @Test
+    public void testIgnoreAnnotation() {
+        System.out.println("This test won't run");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Run after each @Test method");
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        System.out.println("Run once, after all @Test methods have run");
+    }
+
 }
