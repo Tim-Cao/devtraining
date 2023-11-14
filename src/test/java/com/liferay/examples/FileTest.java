8b88fe14b578fd359bf9ffb85eef598a1cfe9b30
Yang Cao <yang.cao@liferay.com>
2023-11-15 03:32:14 +0800

    Add file test

--- src/test/java/com/liferay/examples/FileTest.java
+++ src/test/java/com/liferay/examples/FileTest.java
@@ -1,4 +1,24 @@
 package com.liferay.examples;
 
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
 public class FileTest {
+    @Test
+    public void testAddFileAndDelete() throws IOException {
+        File file = new File(System.getProperty("user.dir") + "temp.text");
+
+        file.createNewFile();
+
+        assertTrue(file.exists());
+
+        file.delete();
+
+        assertFalse(file.exists());
+    }
+
 }
