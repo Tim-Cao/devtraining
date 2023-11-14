5ccedc863be2fa301f3d4d48b98c2b2574c1c4e0
Yang Cao <yang.cao@liferay.com>
2023-11-15 03:51:04 +0800

    Add empty line

--- src/test/java/com/liferay/examples/ConditionTest.java
+++ src/test/java/com/liferay/examples/ConditionTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ConditionTest {
+
     @Test
     public void testAssertURLPattern() {
         String url = "http://www.liferay.com";
