38269175505211b1379934cd5be91b890ebbb80f
Yang Cao <yang.cao@liferay.com>
2023-11-15 03:17:24 +0800

    Add properties test

--- src/test/java/com/liferay/examples/PropertiesTest.java
+++ src/test/java/com/liferay/examples/PropertiesTest.java
@@ -1,4 +1,21 @@
 package com.liferay.examples;
 
+import org.junit.Test;
+
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
 public class PropertiesTest {
+
+    @Test
+    public void testGetProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("title", "Welcome to Liferay");
+        properties.setProperty("viewableBy", "Anyone");
+
+        assertEquals("Welcome to Liferay", properties.getProperty("title"));
+        assertEquals("Anyone", properties.getProperty("viewableBy"));
+    }
+
 }
