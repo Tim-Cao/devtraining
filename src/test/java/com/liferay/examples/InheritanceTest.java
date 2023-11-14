a47ce85ef4b254b82ee46e33797a5b59e4c3957e
Yang Cao <yang.cao@liferay.com>
2023-11-15 03:48:01 +0800

    Add inheritance test

--- src/test/java/com/liferay/examples/InheritanceTest.java
+++ src/test/java/com/liferay/examples/InheritanceTest.java
@@ -1,4 +1,17 @@
 package com.liferay.examples;
 
+import com.liferay.model.AdminUser;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class InheritanceTest {
+
+    @Test
+    public void testGetAdminUserPassword() {
+        AdminUser adminUser = new AdminUser();
+
+        assertEquals("admin", adminUser.getPassword());
+    }
+
 }
